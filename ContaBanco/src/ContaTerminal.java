7360bd0eb6a41416d1d045041b9d5c44c9c2f8e6
SergioCostaAndrade <scandrade1968@gmail.com>
2024-05-05 23:20:42 -0300

    Conta Terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.printf("Hello world");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.printf("Digite o Numero da Conta");
+        int numeroConta = scanner.nextInt();
+        System.out.printf("Digite o Numero da Agência");
+        String numeroAgencia = scanner.next();
+        System.out.printf("Digite o Saldo da Conta");
+        double saldoConta = scanner.nextDouble() ;
+        System.out.printf("Digite o Nome do Cliente");
+        String nomeCliente = scanner.next();
+        System.out.printf("Digite o Sobrenome do Cliente");
+        String sobrenomeCliente = scanner.next();
+        System.out.printf("Olá " +  nomeCliente + " " + sobrenomeCliente +
+        " obrigado por criar uma conta em nosso banco, sua agência é " +
+                numeroAgencia + " conta " +
+                String.valueOf(numeroConta) +
+                " e seu saldo R$ " + String.valueOf(saldoConta) +
+                " já está disponível para saque.");
     }
 }
